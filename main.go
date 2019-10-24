b5153819be09847ff15253aa0bbf402d52ab78a4
Asish Tom <46538895+asish-tom@users.noreply.github.com>
2019-10-24 16:19:06 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
 
+package main
+
+import (
+	"fmt"
+	)
+
+func main()  {
+	greetEmptyUser := "1"
+	fmt.Println("Empty User Greetings:", greetEmptyUser)
+	greetValidUser := "2"
+	fmt.Println("Valid User Greetings:", greetValidUser)
+
+}
